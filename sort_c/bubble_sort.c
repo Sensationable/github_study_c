eff7f21cdbafc08986e702d1f00ea23942127e00
xutianrun <1185081745@qq.com>
2022-06-14 15:01:40 +0800

    c_sort

---
+++ sort_c/bubble_sort.c
@@ -0,0 +1,36 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>	
+
+void bubble_sort(int arr[], int sz)
+{
+	int i = 0;
+	int j = 0;
+    //ÃÀ ˝
+	for (i = 0; i < sz-1; i++)
+	{
+		//“ªÃÀ√∞≈›≈≈–Ú
+		for (j = 0; j < sz - 1 - i; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int tmp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+}
+int main()
+{
+	int arr[10] = { 9,8,7,6,5,4,3,2,1,0 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	//√∞≈›≈≈–Ú µœ÷
+	bubble_sort(arr,sz);
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", arr[i]);
+
+	}
+	return 0;
+}
